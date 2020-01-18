0be3d232e19321b9a6b7b5d41b4efea03e02aa4c
Ziyak Jehangir <ziyak97@gmail.com>
2020-01-19 00:55:02 +0530

    Broken homepage into smaller components, finisdhed styling.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React from 'react'
 import './App.css'
 
-import './homepage.component';
-import { HomePage } from './homepage.component';
+import { HomePage } from './components/pages/homepage/homepage.component'
 
 function App() {
   return (
---
+++ src/components/pages/homepage/homepage.component.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import './homepage.styles.scss'
+
+import Directory from '../../directory/directory.component';
+
+
+export const HomePage = () => {
+    return (
+        <div className="homepage">
+            <Directory />
+        </div>
+    )
+}
